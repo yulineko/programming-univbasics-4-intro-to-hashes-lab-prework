4e39753e63e3c8f0981f8e7c58f70c27d07a6140
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 00:45:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,11 +28,15 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  my_hash_creator = {key => value}
+  my_hash_creator = {
+    key => value
+    
+  }
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  read_from_hash = 
 end
 
 def update_counting_hash(hash, key)
