fb36d5b23b42e3932da2e973d3e38288b361eaf2
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 00:45:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  read_from_hash = 
+  read_from_hash = hash[key]
 end
 
 def update_counting_hash(hash, key)
